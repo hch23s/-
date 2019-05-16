b41ce4f645c52eb12e7adac31a991320d120c1fa
hch23s <hch23s@163.com>
2019-05-16 08:57:09 +0800

    放

--- src/山东省.java
+++ src/山东省.java
@@ -1,11 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class É½¶«Ê¡ {
 
 	public static void main(String[] args) {
-		int i=1;
-		while(++i<3) {
-			System.out.println(i);
-		};
+		int [] a= {12,4};
+		Arrays.sort(a);
+		System.out.println(a);
+	   String string="s";
+	   String string2=new String ("s");
+	   System.out.println(string==string2);
+	   System.out.println(string.equals(string2));
+	   ArrayList<Object> arrayList = new ArrayList<>();
+	   
+	 
+	 if(arrayList.isEmpty()) {
+		 System.out.println(arrayList.size()==0);
+		 System.out.println("2");
+	 }
 	
 	}
 }
